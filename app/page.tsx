a64ec3ea83e3eea5cf128e79a6b761e39348e150
Ravi Kumar <ravi14577@gmail.com>
2024-03-08 12:44:06 +0530

    adding loader and other section ,

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Room } from "./Room";
+import dynamic from "next/dynamic";
 
-export default function Page() {
-  return (
-    <Room>
-      <h1> HII</h1>
-    </Room>
-  );
-}
+/**
+ * disable ssr to avoid pre-rendering issues of Next.js
+ *
+ * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
+ */
+const App = dynamic(() => import("./App"), { ssr: false });
+
+export default App;

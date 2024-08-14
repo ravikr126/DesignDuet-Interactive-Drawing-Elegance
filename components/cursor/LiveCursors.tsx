4f36bea49c88dd893973de41fb8e5c17e20aaef5
ravikr126 <ravi14577@gmail.com>
2024-08-14 15:30:27 +0530

    cursor section complete

--- components/cursor/LiveCursors.tsx
+++ components/cursor/LiveCursors.tsx
@@ -1,14 +1,24 @@
-import { LiveCursorProps } from "@/types/type";
-import React from "react";
+import { LiveCursorProps } from '@/types/type'
+import React from 'react'
+import Cursor from './Cursor';
+import { COLORS } from '@/constants';
 
 const LiveCursors = ({ others }: LiveCursorProps) => {
-  return others.map(({ connectionId, presence }) => {
-    if (presence == null || !presence?.cursor) {
-      return null;
-    } else {
-      return cursor;
-    }
-  });
-};
+    return others.map(({ connectionId, presence }) => {
+        if (presence == null || !presence?.cursor) {
+            return null;
+        }
 
-export default LiveCursors;
+        return (
+            <Cursor
+                key={connectionId}
+                color={COLORS[Number(connectionId) % COLORS.length]}
+                x={presence.cursor.x}
+                y={presence.cursor.y}
+                message={presence.message}
+            />
+        )
+    })
+}
+
+export default LiveCursors
